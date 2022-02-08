6fdb39d3da44aef9f1c43ef9f33d0c0251e254a7
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-08 13:52:49 -0500

    commit tree 3246acbc1340aed0337711f90f1015fc95cd89c5

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const likeButtons = document.querySelectorAll('.like-glyph');
+likeButtons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    const heart = e.target;
+    if (heart.textContent === EMPTY_HEART) {
+      const myPromise = mimicServerCall();
+      myPromise
+      .then(() => {
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+      .catch((error) => {
+        const modal = document.querySelector('#modal');
+        modal.classList.remove('hidden');
+        document.querySelector('#modal_message').textContent = error;
+        setTimeout(() => {
+          modal.classList.add('hidden')
+        }, 3000);
+      })
+     }
+     else {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+     }
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
